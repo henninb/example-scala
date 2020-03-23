080b073e4073f9a71db6327b839c71f07ed400da
Brian Henning <henninb@msn.com>
2020-03-23 16:44:46 -0500

    docker updates

---
+++ src/main/scala/example/services/PersonService.scala
@@ -0,0 +1,19 @@
+package example.services
+
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.stereotype.{Component, Service}
+
+@EnableScheduling
+@Component
+class PersonService {
+
+  import com.fasterxml.jackson.core.JsonProcessingException
+  import org.springframework.scheduling.annotation.Scheduled
+
+  @Scheduled(fixedDelay = 5000)
+  //@throws[JsonProcessingException]
+  def runMe(): Unit = {
+    //val payload = null
+    print("blah")
+  }
+}
