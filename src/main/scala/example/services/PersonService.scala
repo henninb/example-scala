fe67eb99279f6047c2f383cc005f9bf6a5bc92e4
Brian Henning <henninb@msn.com>
2020-05-25 09:28:35 -0500

    updates

--- src/main/scala/example/services/PersonService.scala
+++ src/main/scala/example/services/PersonService.scala
@@ -1,19 +1,15 @@
 package example.services
 
-import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.stereotype.{Component, Service}
+import com.fasterxml.jackson.core.JsonProcessingException
+import org.springframework.scheduling.annotation.Scheduled
 
-@EnableScheduling
 @Component
 class PersonService {
 
-  import com.fasterxml.jackson.core.JsonProcessingException
-  import org.springframework.scheduling.annotation.Scheduled
 
   @Scheduled(fixedDelay = 5000)
-  //@throws[JsonProcessingException]
   def runMe(): Unit = {
-    //val payload = null
     print("scheduling works.\n")
   }
 }
