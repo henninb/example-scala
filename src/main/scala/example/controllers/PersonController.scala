d7187c521291659b4ede022efcdcfc059836d91a
Brian Henning <henninb@msn.com>
2020-08-04 06:55:19 -0500

    updates to controller and health endpoint

--- src/main/scala/example/controllers/PersonController.scala
+++ src/main/scala/example/controllers/PersonController.scala
@@ -1,8 +1,13 @@
 package example.controllers
 
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.{GetMapping, RequestMapping, RestController}
 
 @RestController
+//@RequestMapping(path = Array("/api"))
 class PersonController {
 
+  @GetMapping(path = Array("/api/"))
+  def getUser(): String = {
+    "testUser"
+  }
 }
